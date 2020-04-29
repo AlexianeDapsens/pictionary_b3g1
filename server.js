c78144c5d29a069d29ddb82754c5a6c0b75aed9e
AlexDaps <47327248+AlexDaps@users.noreply.github.com>
2020-04-29 10:34:53 +0200

    Setup pictionary client side

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+//Pour lancer le serveur
+const express = require('express');
+const socketIO = require('socket.io');
+
+const port = 3000;
+const index = '/pictionary.html';
+
+const server = express()
+	.use((req, res) => res.sendFile(index, {root: __dirname}))
+	.listen(port, () => console.log('Listening on port 3000'));
+
+
+
+//Demander à socket IO d'écouter le serveur pour savoir ce qui se passe dessus
+const io = socketIO(server);
+
+//quand il y a une connection, exécuter une fonction
+io.on('connection', (socket) => {
+	console.log('A new client joined the server');
+	onConnection(socket);
+});
+
+function onConnection(socket){
+	socket.on('username', (username) => {
+		console.log('Client name : ', username);
+	});
+
+	socket.on('line', (data) => {
+		socket.broadcast.emit('line', data);
+	});
+}
