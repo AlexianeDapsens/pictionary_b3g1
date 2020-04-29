6178c01d5820214d61a556987a1884cf69b3ea56
AlexDaps <47327248+AlexDaps@users.noreply.github.com>
2020-04-29 11:50:24 +0200

    handle user informations

--- server.js
+++ server.js
@@ -22,12 +22,34 @@ io.on('connection', (socket) => {
 	onConnection(socket);
 });
 
+//tableau pour stocker les users
+let users = [];
+
 function onConnection(socket){
 	socket.on('username', (username) => {
 		console.log('Client name : ', username);
+		//stocker tous les nom d'utilisateurs dans le socket
+		socket.username = username;
+		users.push(socket);
+		sendUsers();
 	});
 
 	socket.on('line', (data) => {
 		socket.broadcast.emit('line', data);
 	});
+
+	//qd 1 client se déconnecte
+	socket.on('disconnect', () => {
+		//parcourir tableau et supprimer celui qui vient de se déconnecter
+		users = users.filter((user) => {
+			return user !== socket;
+		});
+		sendUsers();
+	});
+}
+
+function sendUsers(){
+	io.emit('users', users.map((user) => {
+		return user.username;
+	}));
 }
